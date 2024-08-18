5a722fa6819fa3668da141c4c7cbc9fd4267296f
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-08-18 18:02:44 +0530

    simple to do

--- WEEK_3/TODO/todo.js
+++ WEEK_3/TODO/todo.js
@@ -1,5 +1,19 @@
+let ctr=0;
+function deleteTodo(index){
+    const element=document.getElementById(index);
+    element.parentNode.removeChild(element)
+}
+
 function addTodo(){
+    ++ctr;
     const inputVal=document.querySelector("input");
     const val=inputVal.value;
-    console.log(val)
+    
+    const newTodo=document.createElement("div");
+    newTodo.setAttribute("id",ctr);
+    newTodo.innerHTML= `<div>`  + val + `</div> <button class="btn btn-primary" onClick="deleteTodo(` + ctr + `)">Delete todo</button>`;
+    const parentElement=document.getElementById("todoContainer");
+    
+    parentElement.appendChild(newTodo)
+    inputVal.value = "";
 }
