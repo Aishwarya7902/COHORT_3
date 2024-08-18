0fd15db73555c2f4b268424e5d0ab29529287200
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-08-19 00:36:26 +0530

    todo with complex dom manipulation

--- WEEK_3/TODO/todo.js
+++ WEEK_3/TODO/todo.js
@@ -9,11 +9,19 @@ function addTodo(){
     const inputVal=document.querySelector("input");
     const val=inputVal.value;
     
-    const newTodo=document.createElement("div");
-    newTodo.setAttribute("id",ctr);
-    newTodo.innerHTML= `<div>`  + val + `</div> <button class="btn btn-primary" onClick="deleteTodo(` + ctr + `)">Delete todo</button>`;
-    const parentElement=document.getElementById("todoContainer");
+    const newSpan=document.createElement("span");
+    newSpan.innerHTML=val;
+    newSpan.setAttribute("id", "span-" + ctr);
+
+    const buttonEl=document.createElement("button");
+    buttonEl.innerHTML="Delete"
+    buttonEl.setAttribute("onclick", `deleteTodo('div-${ctr}')`); // Set correct ID
+
+    const newDiv=document.createElement("div");
+    newDiv.setAttribute("id", "div-" + ctr); // Set unique ID
+    newDiv.appendChild(newSpan);
+    newDiv.appendChild(buttonEl);
+    document.getElementById("todoContainer").appendChild(newDiv)
     
-    parentElement.appendChild(newTodo)
     inputVal.value = "";
 }
