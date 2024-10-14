c197bccce4cd09450d39ecf3425e670eb54e042f
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-10-14 17:44:06 +0530

    UseState via Post component example

--- WEEK_9/week-9-offline/src/App.jsx
+++ WEEK_9/week-9-offline/src/App.jsx
@@ -1,34 +1,46 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { PostComponent } from './Post'
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [posts,setPosts]=useState([]);
+
+  const postComponents=posts.map(post=>
+    <PostComponent
+    name={post.name}
+    subtitle={post.subtitle}
+    time={post.title}
+    image={post.image}
+    description={post.description}
+    
+    />
+  )
+  function addPost() {
+    setPosts([...posts,{
+      name: "harkirat",
+      subtitle: "10000 followers",
+      time: "2m ago",
+      image: "https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg",
+      description: "What to know how to win big? Check out how these folks won $6000 in bounties."
+    }])
+  }
 
   return (
-    
     <div style={{
-      background:"#dfe6e9",
-      height:"100vh",
-      width:"100vw"
-      
+      background: "#dfe6e9",
+      height: "100vh",
+      width: "100vw"
     }}>
-       <ToggleMeassge/> 
+      <button onClick={addPost}>Add Post</button>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div>
+          {postComponents}
+        </div>
+      </div>
+
     </div>
+
   )
 }
 
-const ToggleMeassge =()=>{
-  const [notificationCount,setNotifactionCount]=useState(0);
-  function increment(){
-    setNotifactionCount(notificationCount+1)
-  }
-  return(
-    <div>
-        <button onClick={increment}>Toggle Message</button>
-        {notificationCount}
-    </div>
-  )
-}
+
 export default App
