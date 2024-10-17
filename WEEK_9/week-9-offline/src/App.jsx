a1182c297db9e48fd082df00b03a986d6c389129
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-10-17 19:04:37 +0530

    understanding keys in react

--- WEEK_9/week-9-offline/src/App.jsx
+++ WEEK_9/week-9-offline/src/App.jsx
@@ -2,18 +2,13 @@ import { useEffect, useState } from 'react'
 
 function App() {
 
-const todos=[{
-  title:"week 9.4",
-  done:false
-},{
-  title:"web3 week 4",
-  done:true
-}]
 
-const todosComponents=todos.map(todo=><Todo title={todo.title} done={todo.done}/>)
+
+
   return (
     <div>
-        {todosComponents}
+        <Todo  key={1} title={"Go to gym"} done={false}/>
+        <Todo key={2} title={"Go to study"} done={true}/>
     </div>
 
   )
