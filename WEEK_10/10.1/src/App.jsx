b3d778656bf06ff7f8a3aabd6f5b30e3f0a771f4
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-10-20 17:29:17 +0530

    Layouts in react

--- WEEK_10/10.1/src/App.jsx
+++ WEEK_10/10.1/src/App.jsx
@@ -1,41 +1,55 @@
 
 import './App.css'
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom"
 function App() {
 
   return <div>
     <BrowserRouter>
-      <Link to="/">Allen</Link>
-      | 
-      <Link to="/neet/online-coaching-class-11">Class 11</Link> 
-      | 
-      <Link to="/neet/online-coaching-class-12">Class 12</Link>
+      
       <Routes>
-        <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
-        <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
-        <Route path="/" element={<Landing />} />
+        <Route path='/' element={<Layout/>}>
+          <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
+          <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+
       </Routes>
     </BrowserRouter>
   </div>
 }
 
+function Layout(){
+  return <div>
+     <Link to={"/"}>Allen</Link>
+      |
+      <Link to={"/neet/online-coaching-class-11"}>Class11</Link>
+      |
+      <Link to={"/neet/online-coaching-class-12"}>Class12</Link>
+     <Outlet/>
+     Footer
+  </div>
+}
 function Landing() {
   return <div>
-    Welcome to allen
+    Welcome to Allen
   </div>
 }
 
 function Class11Program() {
   return <div>
-      NEET programs for Class 11th
+    Prepare for NEET Programs for Class 11th
   </div>
 }
-
 function Class12Program() {
   return <div>
-      NEET programs for Class 12th
+    Prepare for NEET Programs for Class 12th
   </div>
 }
 
+function ErrorPage() {
+  return <div>
+    Sorry !! Page doesn't Exist
+  </div>
+}
 export default App
