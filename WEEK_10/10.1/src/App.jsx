ce97ac1678a2d9a8b8575a2f8968822b8043873d
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-10-21 13:21:27 +0530

    making some corrections in layout

--- WEEK_10/10.1/src/App.jsx
+++ WEEK_10/10.1/src/App.jsx
@@ -1,12 +1,54 @@
 
+
 import './App.css'
-import FocusInput from './components/FocusInput'
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom"
 function App() {
 
   return <div>
-    <FocusInput/>
+    <BrowserRouter>
+      
+      <Routes>
+        <Route path='/neet' element={<Layout/>}>
+          <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
+          <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
   </div>
 }
 
+function Layout(){
+  return <div>
+     <Link to={"/"}>Allen</Link>
+      |
+      <Link to={"/neet/online-coaching-class-11"}>Class11</Link>
+      |
+      <Link to={"/neet/online-coaching-class-12"}>Class12</Link>
+     <Outlet/>
+     Footer
+  </div>
+}
+function Landing() {
+  return <div>
+    Welcome to Allen
+  </div>
+}
 
+function Class11Program() {
+  return <div>
+    Prepare for NEET Programs for Class 11th
+  </div>
+}
+function Class12Program() {
+  return <div>
+    Prepare for NEET Programs for Class 12th
+  </div>
+}
+
+function ErrorPage() {
+  return <div>
+    Sorry !! Page doesn't Exist
+  </div>
+}
 export default App
