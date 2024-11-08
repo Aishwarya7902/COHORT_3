840253c0fe8b320b84b9e85283a156cdd7732003
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-11-08 23:43:57 +0530

    usePrev custom hook

--- WEEK_11/custom_hooks/src/App.jsx
+++ WEEK_11/custom_hooks/src/App.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react'
 import './App.css'
+import { usePrev } from './hooks/usePrev';
 
 function App() {
  
-
+  const [count,setCount]=useState(0);
+  const prev=usePrev(count);
   return (
     <>
-      
+      <p>{count}</p>
+      <button onClick={()=>{
+        setCount(curr=>curr+1);
+      }}>Click Me</button>
+      <p>The previous value was {prev}</p>
     </>
   )
 }
