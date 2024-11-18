48ccd9f60b1e56d628891a154d15e9a0fde00180
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2024-11-18 20:49:48 +0530

    Memo in recoil

--- WEEK_11/recoil/src/App.jsx
+++ WEEK_11/recoil/src/App.jsx
@@ -1,53 +1,53 @@
 import './App.css'
 import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
 import { counterAtom } from './store/atoms/counterAtom';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { memo } from 'react';
 function App() {
-  
 
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    setInterval(() => {
+      setCount(c => c + 1)
+    }, 3000)
+  }, [])
   return (
     <>
-      <RecoilRoot>
-         <Counter/>
-      </RecoilRoot>
+      <CurrentCount count={count}/>
+      <Increase />
+      <Decrease />
     </>
   )
 }
 
-function Counter(){
-  return <>
-     <CurrentCount/>
-     <Increase/>
-     <Decrease/>
-  </>
-}
 
-function CurrentCount(){
-  const count=useRecoilValue(counterAtom)
+
+const CurrentCount = memo(function ({count}) {
+
   return <>
     {count}
   </>
-}
+})
 
-function Increase(){
-  const setCount=useSetRecoilState(counterAtom)
-  function handleIncrease()
-  {
-    setCount(c=>c+1);
+const Increase = memo(function () {
+
+  function handleIncrease() {
+    console.log("Hi from increase")
   }
   return <>
     <button onClick={handleIncrease}>Increase</button>
   </>
-}
+})
 
-function Decrease(){
-  const setCount=useSetRecoilState(counterAtom)
-  function handleDecrease()
-  {
-    setCount(c=>c-1);
+const Decrease = memo(function () {
+
+  function handleDecrease() {
+    console.log("Hi from decrease")
   }
   return <>
     <button onClick={handleDecrease}>Decrease</button>
   </>
-}
+})
 
 export default App
