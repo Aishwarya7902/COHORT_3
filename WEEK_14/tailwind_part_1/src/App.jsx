e838725fe1edc7c50c376690b14ba6c65a2c796f
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2025-01-02 22:24:06 +0530

    Initial Commit for #project1

---
+++ WEEK_14/tailwind_part_1/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Button from './components/Button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+
+    <div>
+      
+      <Button disabled={false}>Sign Up </Button>
+    </div>
+
+  )
+}
+
+export default App
---
+++ WEEK_14/tailwind_part_1/src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Button = ({
+    disabled,
+    children,
+    onClick
+}) => {
+  return (
+    <span onClick={onClick} className={`text-white rounded-2xl font-4xl px-32 py-8 cursor-pointer ${disabled ? "bg-blue-200" :"bg-green-400"}`}>
+     {children}
+    </span>
+  )
+}
+
+export default Button
